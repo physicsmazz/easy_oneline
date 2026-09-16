941b5a3efa55f748fa5a075f4392e0981ed7a6c4
physicsmazz <physicsmazz@gmail.com>
2026-09-16 11:12:23 -0400

    Align target selection highlight

--- Easy1Line/ContentView.swift
+++ Easy1Line/ContentView.swift
@@ -1158,6 +1158,7 @@ private struct TargetView: View {
                     Circle().stroke(.cyan, lineWidth: 3).frame(width: 30, height: 30).shadow(color: .cyan.opacity(0.8), radius: 8)
                 } else {
                     RoundedRectangle(cornerRadius: 12).stroke(.cyan, lineWidth: 3).frame(width: 66, height: 56).shadow(color: .cyan.opacity(0.8), radius: 8)
+                        .offset(y: -14)
                 }
             }
         }
