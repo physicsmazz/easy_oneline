6b61e5fca2d0a1cd0d63f37e35e67b2df60801d7
physicsmazz <physicsmazz@gmail.com>
2026-09-16 09:18:38 -0400

    Initial commit

---
+++ Easy1Line/Easy1LineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy1LineApp.swift
+//  Easy1Line
+//
+//  Created by Mike Mazzantini on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Easy1LineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
